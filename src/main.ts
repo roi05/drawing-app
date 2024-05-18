347208f4f797555d2ad1c87f41729321d4bafa9e
roi <roibuenaventura05@gmail.com>
2024-05-18 21:08:59 +0800

    implement drawing square

--- src/main.ts
+++ src/main.ts
@@ -3,64 +3,115 @@ import './style.css';
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
+type Path = {
+  type: 'line' | 'square';
+  points: {
+    x: number;
+    y: number;
+    width?: number;
+    height?: number;
+    propery: { color: string };
+  }[];
+};
+
 let isDrawing: boolean = false;
 let isErasing: boolean = false;
+
 let lastX: number = 0;
 let lastY: number = 0;
-let paths: {
-  points: { x: number; y: number; propery?: { color?: string } }[];
-}[] = [];
+let firstX: number = 0;
+let firstY: number = 0;
+
+let drawingType: 'line' | 'square' | undefined = 'line';
+
+let paths: Path[] = [];
 
 function resizeCanvas(): void {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  redrawPaths();
+  redraw();
 }
 
 function stopDrawing(): void {
+  if (!isDrawing) return;
+
+  const width = lastX - firstX;
+  const height = lastY - firstY;
+
+  if (drawingType === 'square') {
+    paths.push({
+      type: 'square',
+      points: [
+        {
+          x: firstX,
+          y: firstY,
+          width,
+          height,
+          propery: {
+            color: 'white',
+          },
+        },
+      ],
+    });
+    savePathsToLocalStorage();
+    redraw();
+  }
+
   isDrawing = false;
 }
 
-function startDrawing(event: MouseEvent): void {
+function startDraw(event: MouseEvent): void {
   isDrawing = true;
   lastX = event.offsetX;
   lastY = event.offsetY;
-  paths.push({
-    points: [
-      {
-        x: lastX,
-        y: lastY,
-        propery: {
-          color: isErasing ? 'black' : 'white',
+  firstX = event.offsetX;
+  firstY = event.offsetY;
+
+  if (drawingType === 'line') {
+    paths.push({
+      type: 'line',
+      points: [
+        {
+          x: lastX,
+          y: lastY,
+          propery: {
+            color: isErasing ? 'black' : 'white',
+          },
         },
-      },
-    ],
-  });
+      ],
+    });
+  }
 }
 
-function drawLine(event: MouseEvent): void {
+function drawing(event: MouseEvent): void {
   if (!isDrawing) return;
 
   const currentX = event.offsetX;
   const currentY = event.offsetY;
 
-  paths[paths.length - 1].points.push({
-    x: currentX,
-    y: currentY,
-    propery: {
-      color: isErasing ? 'black' : 'white',
-    },
-  });
+  if (drawingType === 'line') {
+    paths[paths.length - 1].points.push({
+      x: currentX,
+      y: currentY,
+      propery: {
+        color: isErasing ? 'black' : 'white',
+      },
+    });
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
 
-  ctx.strokeStyle = isErasing ? 'black' : 'white';
-  ctx.lineWidth = 5;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(currentX, currentY);
+    ctx.stroke();
+  }
 
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(currentX, currentY);
-  ctx.stroke();
+  if (drawingType === 'square') {
+    redraw();
+    ctx.strokeStyle = 'gray';
+    ctx.strokeRect(firstX, firstY, lastX - firstX, lastY - firstY);
+  }
 
   lastX = currentX;
   lastY = currentY;
@@ -68,20 +119,29 @@ function drawLine(event: MouseEvent): void {
   savePathsToLocalStorage();
 }
 
-function redrawPaths(): void {
+function redraw(): void {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   paths.forEach(path => {
-    ctx.lineWidth = 5;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
+    if (path.type === 'line') {
+      ctx.lineWidth = 5;
+      ctx.lineJoin = 'round';
+      ctx.lineCap = 'round';
 
-    ctx.beginPath();
-    ctx.moveTo(path.points[0].x, path.points[0].y);
-    path.points.slice(1).forEach(point => {
-      ctx.strokeStyle = point?.propery?.color || 'white';
-      ctx.lineTo(point.x, point.y);
-    });
-    ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(path.points[0].x, path.points[0].y);
+      path.points.slice(1).forEach(point => {
+        ctx.strokeStyle = point?.propery?.color || 'white';
+        ctx.lineTo(point.x, point.y);
+      });
+      ctx.stroke();
+    }
+
+    if (path.type === 'square') {
+      path.points.forEach(rect => {
+        ctx.strokeStyle = rect.propery.color;
+        ctx.strokeRect(rect.x, rect.y, rect.width || 0, rect.height || 0);
+      });
+    }
   });
 }
 
@@ -93,14 +153,14 @@ function loadPathsFromLocalStorage(): void {
   const savedPaths = localStorage.getItem('drawnPaths');
   if (savedPaths) {
     paths = JSON.parse(savedPaths);
-    redrawPaths();
+    redraw();
   }
 }
 
 function undoLastPath(): void {
   if (paths.length > 0) {
     paths.pop();
-    redrawPaths();
+    redraw();
     savePathsToLocalStorage();
   }
 }
@@ -110,8 +170,8 @@ loadPathsFromLocalStorage();
 
 window.addEventListener('resize', resizeCanvas);
 
-canvas.addEventListener('mousedown', startDrawing);
-canvas.addEventListener('mousemove', drawLine);
+canvas.addEventListener('mousedown', startDraw);
+canvas.addEventListener('mousemove', drawing);
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mouseout', stopDrawing);
 
